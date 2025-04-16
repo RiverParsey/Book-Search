16fe821f5179449ace5d422fb859ea90e9d8cdfa
ricsi <rihardptrk18@gmail.com>
2025-04-16 11:39:15 +0200

    task-3_added_comments

--- src/js/modules/request.js
+++ src/js/modules/request.js
@@ -1,56 +1,60 @@
 function request(inputSelector, btnSelector, resultField) {
-  const input = document.querySelector(inputSelector);
-  const btn = document.querySelector(btnSelector);
-  const result = document.querySelector(resultField);
+  const input = document.querySelector(inputSelector); // получение элемента ввода текста
+  const btn = document.querySelector(btnSelector); // получение элемента кнопки отправки
+  const result = document.querySelector(resultField); // получение элемента блока вывода результата
 
+  // функция обработчик функционала
   const handleRequest = () => {
-    console.clear();
+    console.clear(); // очистка консоли
 
-    const query = input.value.trim();
+    const query = input.value.trim(); // создание переменной с value элемента ввода
 
+    // проверка на пустое поле
     if (!query) {
       alert('Введите название книги')
       return;
     }
 
-    const encodeQuery = encodeURIComponent(query);
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeQuery}&maxResults=10`;
+    const encodeQuery = encodeURIComponent(query); // создание переменной с кодировкой
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeQuery}&maxResults=10`; // создание переменной с адресом будущего запроса
   
+    // инициализация запроса
     fetch(url)
-      .then(res => res.json())
+      .then(res => res.json()) // обработка результата
       .then(data => {
-        result.innerHTML = '';
-        input.value = '';
+        result.innerHTML = ''; // очистка блока с результатом при каждом новом запросе
+        input.value = ''; // очиста value элемента ввода при каждом новом запросе
   
-        console.log(data);
+        console.log(data); // вывод в консоль для проверки работы
   
-        if (data.items && data.items.length > 0) {
-          data.items.forEach(item => {
-            const info = item.volumeInfo;
-            const bookEl = document.createElement('div');
-            bookEl.classList.add('card');
-  
+        if (data.items && data.items.length > 0) { // условие проверки наличия данных
+          data.items.forEach(item => { // инициализация перебора данных
+            const info = item.volumeInfo; // создание переменной с полученными данными
+            const bookEl = document.createElement('div'); // создание элемента для вывода данных на страницу
+            bookEl.classList.add('card'); // добавление класса к элементу
+            
+            // создание html структуры элемента
             bookEl.innerHTML = `
               <img src="${info.imageLinks.thumbnail || ''}" alt="book-cover">
               <div class="title">${info.title || 'Заголовок не найден'}</div>
               <div class="subtitle">${info.subtitle || 'Подзаголовок не найден'}</div>
               <div class="subtitle">${info.description || 'Описание не найдено'}</div>
             `;
-            result.appendChild(bookEl);
+            result.appendChild(bookEl); // добавление созданного элемента в блок с результатом
           })
   
         } else {
-          result.innerHTML = 'Книга не найдена';
+          result.innerHTML = 'Книга не найдена'; // блок кода else если данных все же не было
         }
       })
       .catch(() => {
-        alert('Ошибка запроса');
+        alert('Ошибка запроса'); // обработка ошибки при запросе
       });
   };
 
-  btn.addEventListener('click', handleRequest);
+  btn.addEventListener('click', handleRequest); // обработчик события клика по кнопке с запуском основной функции обработки функционала
 
-  input.addEventListener('keydown', (event) => {
+  input.addEventListener('keydown', (event) => { // обработчик события нажатия на enter с запуском основной функции обработки функционала
     if (event.key === 'Enter') {
       handleRequest();
     }
@@ -58,4 +62,9 @@ function request(inputSelector, btnSelector, resultField) {
 
 };
 
-export default request;
+export default request; // експорт функции
+
+
+// clear() - очищает консоль в браузере
+// trim() - удаляет пробелы в начале и в конце строки
+// encodeURIComponent() - кодирует строку в безопасный вид для url
